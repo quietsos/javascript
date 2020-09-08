badf1e70f13307d84c29d2c013eb860283363e08
quietsos <shohaniu8@gmail.com>
2020-09-08 13:31:14 +0600

    access javascript file in html file from external folder

---
+++ js/javascript.js
@@ -0,0 +1,14 @@
+function lightOn(){
+document.getElementById('light').src="/media/sos/_dev_sda5/javascript/javascript/images/on.jpeg";
+}
+function lightOff(){
+document.getElementById('light').src="/media/sos/_dev_sda5/javascript/javascript/images/off.png";
+}
+
+function onMessage(){
+	document.getElementById('message').innerHTML="Light is on.";
+}
+
+function onMessage(){
+	document.getElementById('message').innerHTML="Light is off";
+}
